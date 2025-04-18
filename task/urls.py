c8ad2770797f17b630e33049dc3b4dccd4782776
Кашина Екатерина Максимовна <KashinaEM@sood.ru>
2025-04-18 12:53:36 +0400

    you can change task's status now

--- task/urls.py
+++ task/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import task_list
+from .views import task_list, update_task_status
 
 urlpatterns = [
     path('tasks/', task_list, name='task_list'),
 
+path('tasks/update_task_status/<int:task_id>/', update_task_status, name='update_task_status'),
 ]
--- task/views.py
+++ task/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+
 from django.shortcuts import render
-from .models import Task
+from .models import Task, TaskStatus
+from django.http import JsonResponse
+
+from django.views.decorators.csrf import csrf_exempt
 
 def task_list(request):
-    tasks = Task.objects.all()  # Получаем все задачи
-    return render(request, 'accounts/all_tasks.html', {'tasks': tasks})
+    tasks = Task.objects.all()
+    statuses = TaskStatus.objects.all()
+    return render(request, 'accounts/all_tasks.html', {
+        'tasks': tasks,
+        'statuses': statuses
+    })
+
+
+@csrf_exempt
+def update_task_status(request, task_id):
+    if request.method == 'POST':
+        try:
+            task = Task.objects.get(id=task_id)
+            data = json.loads(request.body)
+            new_status_id = data.get('status_id')
+
+            if new_status_id:
+                new_status = TaskStatus.objects.get(id=new_status_id)
+                task.status = new_status
+                task.save()
+                return JsonResponse({'success': True})
+
+            return JsonResponse({'success': False, 'error': 'No status_id provided'})
+        except Task.DoesNotExist:
+            return JsonResponse({'success': False, 'error': 'Task not found'})
+        except TaskStatus.DoesNotExist:
+            return JsonResponse({'success': False, 'error': 'Status not found'})
+        except Exception as e:
+            return JsonResponse({'success': False, 'error': str(e)})
+
+    return JsonResponse({'success': False, 'error': 'Invalid request method'})
